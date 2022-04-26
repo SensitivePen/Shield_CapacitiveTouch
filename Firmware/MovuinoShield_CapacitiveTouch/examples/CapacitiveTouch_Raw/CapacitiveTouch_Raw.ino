9c48bfac9a806127d5536fe928d475643bf8d71c
Wim <poignon.wim@gmx.de>
2022-04-26 16:56:05 +0200

    fix: invert rx tx

--- Firmware/MovuinoShield_CapacitiveTouch/examples/CapacitiveTouch_Raw/CapacitiveTouch_Raw.ino
+++ Firmware/MovuinoShield_CapacitiveTouch/examples/CapacitiveTouch_Raw/CapacitiveTouch_Raw.ino
@@ -1,23 +1,30 @@
 #include "_CapacitiveTouch.h"
 
+#define NUM_TX_USED             4
+#define NUM_RX_USED             6
+
 CapacitiveTouch capa=CapacitiveTouch();
 
 void setup() {
   Serial.begin(115200);
-  capa.begin();
   while (!Serial);
-  Serial.println("CapacitiveTouch get Raw data");
+  if (DEBUG) Serial.println("CapacitiveTouch get Raw data");
+  capa.init();
+  delay(1000);
+  capa.calibrate();
 }
 
 void loop() {
   String raw="";
-  if (capa.updated()) {
-   for (int i = 0; i < ROWS_USE * NUM_COLUMNS; i++) {
-      if (capa.grid[i] > 0)raw+=capa.grid[i];
-      if (i != ROWS_USE * NUM_COLUMNS - 1)
-        raw+=",";
+  if(capa.updated()) {
+    for (int txAddr=0;txAddr<NUM_TX_USED;txAddr++){
+      for(int rxAddr=0;rxAddr<NUM_RX_USED;rxAddr++){
+          raw+=capa.grid[txAddr][rxAddr];
+          raw+=",";
+      }
     }
+    raw.remove(raw.length()-1);
+    Serial.println(raw);
   }
-  Serial.println(raw+";");
-  delay(1000);
+  delay(1);
 }
