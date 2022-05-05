21c6daeeff26fcb00d5ef9365ec75e19baa716cf
Wim <poignon.wim@gmx.de>
2022-05-05 17:35:21 +0200

    fix(lib): SDA/SCL ESP32

--- Firmware/MovuinoShield_CapacitiveTouch/examples/CapacitiveTouch_Raw/CapacitiveTouch_Raw.ino
+++ Firmware/MovuinoShield_CapacitiveTouch/examples/CapacitiveTouch_Raw/CapacitiveTouch_Raw.ino
@@ -3,14 +3,25 @@
 #define NUM_TX_USED             4
 #define NUM_RX_USED             6
 
+#ifdef _ESP32_HAL_I2C_H_
+#define SDA_PIN                 21
+#define SCL_PIN                 22
+#endif
+
 #define GAIN                    50
 
-CapacitiveTouch capa=CapacitiveTouch();
+CapacitiveTouch capa;
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
   if (DEBUG) Serial.println("CapacitiveTouch get Raw data");
+
+  #ifdef _ESP32_HAL_I2C_H_ // For ESP32
+  Wire.begin(SDA_PIN, SCL_PIN);
+  capa.setWire(&Wire);
+  #endif
+
   capa.init();
   capa.setGain(GAIN);
   delay(100);
--- Firmware/MovuinoShield_CapacitiveTouch/examples/CapacitiveTouch_Raw_AutoCalibration/_CapacitiveTouch.h
+++ Firmware/MovuinoShield_CapacitiveTouch/examples/CapacitiveTouch_Raw_AutoCalibration/_CapacitiveTouch.h
@@ -6,24 +6,23 @@
 // https://github.com/optisimon/ft5406-capacitive-touch/blob/master/CapacitanceVisualizer/FT5406.hpp
 // https://github.com/hyvapetteri/touchscreen-cardiography
 
-#include "Wire.h"
+#include <Arduino.h>
+#include <Wire.h>
 
-const uint16_t I2C_ADDRESS = 0x38;
+#define I2C_ADDRESS         0x38
 
-#define MODE_TEST           0x40
-#define DEBUG               false
+#define DEBUG               true
 
 // RAW
 #define NUM_TX             21
 #define NUM_RX             12
 
-
-#define CALIBRATION_MAX     15
-#define CALIB_THRESHOLD     0 
-
 class CapacitiveTouch {
     public:
-    CapacitiveTouch();
+    const uint8_t address;
+    CapacitiveTouch(uint8_t address=I2C_ADDRESS);
+    void setWire(TwoWire *wire);
+    void setGain(uint8_t val);
     bool init();
     bool isInit = false;
 
@@ -33,169 +32,14 @@ class CapacitiveTouch {
     short grid[NUM_TX][NUM_RX];
     short calibrateGrid[NUM_TX][NUM_RX];
     void calibrate(float variance);
-    void setGain(int val);
-
 
     private:
+    TwoWire* myWire;
     void autoCalib();
     void getRawData();
     int calibrationSteps = 0;
     bool calibrationDone = false;
+    void beginWireIfNull();
+    uint8_t i2cWriteByte(uint8_t Address, uint8_t Register, uint8_t Data);
+    uint8_t i2cRead(uint8_t Address, uint8_t Register, uint8_t NBytes, byte *Data);
 };
-
-CapacitiveTouch::CapacitiveTouch(){}
-
-//// ============================== INIT ==============================
-
-bool CapacitiveTouch::init(){
-
-    // Setup I2C
-    //digitalWrite(SDA,LOW);
-    //digitalWrite(SCL,LOW);
-
-    Wire.begin();
-    Wire.setClock(100000); // 400000 https://www.arduino.cc/en/Reference/WireSetClock
-
-    // Initialization: set device mode to test mode
-    int stat = 0;
-    Wire.beginTransmission(I2C_ADDRESS);
-    Wire.write(byte(0x00));
-    Wire.write(byte(MODE_TEST));
-    stat = Wire.endTransmission(I2C_ADDRESS);
-
-    if(stat==0) isInit = true;
-    else if(DEBUG) {
-        Serial.print("CapacitiveTouch status:");
-        if (stat == 1 ) Serial.println("1: too long for the buffer");
-        if (stat == 2 ) Serial.println("2: NACK  transmit");
-        if (stat == 3 ) Serial.println("3: NACK transmit data");
-        if (stat == 4 ) Serial.println("4: other ");
-    }
-
-    if(DEBUG && isInit) Serial.println("CapacitiveTouch initialized.");
-    else if (DEBUG) Serial.println("CapacitiveTouch not init");
-    
-    // Init calibrateGrid to zero
-    for (unsigned int rxAddr=0;rxAddr<NUM_RX;rxAddr++){
-        for(unsigned int txAddr=0;txAddr<NUM_TX;txAddr++){
-                calibrateGrid[txAddr][rxAddr]=0;
-        }
-    }
-    // setGain(100);
-    return isInit;
-}
-
-// NOT working -> calibration not finished
-void CapacitiveTouch::autoCalib(){
-    if (DEBUG) Serial.println("Autocalibration start.");
-    Wire.beginTransmission(I2C_ADDRESS);
-    Wire.write(byte(0x2));
-    Wire.write(byte(0x4));
-    Wire.endTransmission(I2C_ADDRESS);
-
-    delay(300);
-    for(int i=0;i<100;i++){
-        Wire.beginTransmission(I2C_ADDRESS);
-        Wire.write(0x00);
-        Wire.requestFrom(I2C_ADDRESS,uint8_t(1),false);
-
-        byte reading = Wire.read();
-
-        if ( ((reading & 0x70) >> 4) == 0x0)  // calibration finish
-        {
-            if (DEBUG) Serial.println("Autocalibration done.");
-            break;
-        }
-        delay(100);
-    }
-    // save calibration result
-    Wire.beginTransmission(I2C_ADDRESS);
-    Wire.write(byte(0x02));
-    Wire.write(byte(0x5));
-    Wire.endTransmission(I2C_ADDRESS);
-}
-
-//// ============================== UPDATE ==============================
-
-bool CapacitiveTouch::updated(){
-    if (!isInit) return false;
-    else {
-        getRawData();
-        return true;
-  }
-}
-
-void CapacitiveTouch::getRawData(){
-
-    // Start scan
-    Wire.beginTransmission(I2C_ADDRESS);
-    Wire.write(byte(0x00));
-    Wire.write(byte(0xc0));
-    Wire.endTransmission();
-
-    // Read data
-    for(unsigned int txAddr=0;txAddr<NUM_TX;txAddr++){
-        byte result[2 * NUM_RX];
-
-        // Start transmission
-        Wire.beginTransmission(I2C_ADDRESS);
-        Wire.write(byte(0x01));
-        Wire.write(txAddr);
-        unsigned int st = Wire.endTransmission();
-        if (st < 0 && DEBUG) Serial.println("i2c write failed");
-
-        delayMicroseconds(100); // Wait at least 50us
-
-        Wire.beginTransmission(I2C_ADDRESS);
-        Wire.write(0x10);
-        Wire.endTransmission(false);
-        Wire.requestFrom(I2C_ADDRESS,uint8_t(2*NUM_RX),false);
-        unsigned int g = 0;
-        while (Wire.available()) {
-            result[g++] = Wire.read();
-        }
-
-        for (unsigned int rxAddr=0;rxAddr<NUM_RX;rxAddr++){
-            unsigned int output = (result[2 * rxAddr] << 8) | (result[2 * rxAddr + 1]);
-
-            if(calibrationSteps == CALIBRATION_MAX){
-                grid[NUM_TX-1-txAddr][rxAddr]=output-calibrateGrid[NUM_TX-1-txAddr][rxAddr]+CALIB_THRESHOLD;
-            } else {
-                grid[NUM_TX-1-txAddr][rxAddr]=output;
-            }
-        }
-    }
-}
-
-//// ============================== CALIBRATION ==============================
-
-void CapacitiveTouch::calibrate(float variance=0.){
-    while (calibrationSteps != CALIBRATION_MAX){
-        getRawData();
-        if (grid[0][0] < 5000) return;
-        if (calibrationSteps == 0 && !calibrationDone){
-            memcpy(calibrateGrid,grid,sizeof(grid));
-        } else {
-                for (unsigned int rxAddr=0;rxAddr<NUM_RX;rxAddr++){
-                    for(unsigned int txAddr=0;txAddr<NUM_TX;txAddr++){
-                        calibrateGrid[txAddr][rxAddr]=(grid[txAddr][rxAddr]+calibrateGrid[txAddr][rxAddr])/2;
-                    }
-                }
-        }
-        calibrationSteps++;
-    }
-    calibrationDone = true;
-    if (DEBUG) Serial.println("Calibration done");
-    delay(1000);
-}
-
-void CapacitiveTouch::setGain(int gain) {
-    Wire.beginTransmission(I2C_ADDRESS);
-    Wire.write(byte(0x07));
-    Wire.write(byte(gain));
-    Wire.endTransmission(I2C_ADDRESS);
-    if(DEBUG){
-        Serial.print("Gain set to ");
-        Serial.println((gain));
-    }
-}
